da62248c831b797a5ce7f6030aca4a94a98f7a57
Joao Ferreira <joaovlima07@hotmail.com>
2021-09-18 20:38:52 -0300

    feat: template to pdf

--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -50,6 +50,27 @@ export const handle = async (event) => {
 
     const content = await compile(data)
 
+    const browser = await chromium.puppeteer.launch({
+        headless: true,
+        args: chromium.args,
+        defaultViewport: chromium.defaultViewport,
+        executablePath: await chromium.executablePath
+    })
+
+    const page = await browser.newPage()
+
+    await page.setContent(content)
+
+    await page.pdf({
+        format: 'a4',
+        landscape: true,
+        printBackground: true,
+        preferCSSPageSize: true,
+        path: process.env.IS_OFFLINE ? 'certificate.example.pdf' : null
+    })
+
+    await browser.close()
+
     return {
         statusCode: 201,
         body: JSON.stringify({
